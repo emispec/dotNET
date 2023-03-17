2e8f3a24261feb72986c2f31bb488bfd86084023
emispec <emisebastian86@gmail.com>
2023-03-17 18:52:00 -0300

    S2E3

--- S2E3/Program.cs
+++ S2E3/Program.cs
@@ -11,3 +11,14 @@ Se cumple una de dos condiciones a la vez (una true y otra false)
 
 Nota: puedes escribir estos datos por consola si prefieres verlos. La idea del ejercicio es almacenar los datos en los tipos más adecuados.
 */
+
+int num = 12;
+Console.WriteLine(num>=18);
+
+const char a = 'a';
+char letra = 'a';
+Console.WriteLine(letra == a);
+
+Console.WriteLine((a == letra) && (num >= 5));
+
+Console.WriteLine((3 > 2) && (6 == 4));
