d867107720cbd8314f661fe6f05bf8d091811554
emispec <emisebastian86@gmail.com>
2023-03-22 12:03:24 -0300

    poo1

--- P3 Funciones y metodos II/Program.cs
+++ P3 Funciones y metodos II/Program.cs
@@ -1,1 +1,44 @@
 ﻿
+Puerta door = new Puerta(150, 200, 300, false);
+door.MostrarEstado();
+
+Puerta door2 = new Puerta();
+door2.MostrarEstado();
+
+class Puerta 
+{
+    //ATRIBUTOS
+    int ancho;
+    int alto;
+    int color;
+    bool abierto;
+
+    //CONSTRUCTOR/ES
+       public Puerta() 
+    {
+        ancho = 400;
+        alto = 800;
+        color = 8989;
+        abierto = true;
+    }
+    public Puerta (int ancho, int alto, int color, bool abierto)
+    {
+        this.ancho = ancho;
+        this.alto = alto;
+        this.color = color;
+        this.abierto = abierto;
+    }
+    //METODOS
+    public void MostrarEstado()
+    {
+        Console.WriteLine("{0}", ancho);
+        Console.WriteLine("{0}", alto);
+        Console.WriteLine("{0}", color);
+        Console.WriteLine("{0}", abierto);
+    }
+
+    //DESTRUCTOR
+    ~Puerta() {
+        Console.WriteLine("Puerta destruida !");
+    }
+}
