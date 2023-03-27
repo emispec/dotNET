3ad6593bcec2251a6605563b5eb81c35bbab5da1
emispec <emisebastian86@gmail.com>
2023-03-26 23:53:27 -0300

    IO txt

--- P12 Uso de Archivos/Program.cs
+++ P12 Uso de Archivos/Program.cs
@@ -1,10 +1,31 @@
-﻿//Crear archivos TXT y realizar operaciones
+﻿// TXT: creacion, escritura y lectura
 
 //importar libreria input/output
 using System.IO;
 
-string texto;
+/*
+Los archivos podemos abrirlos en modo lectura, modo escitura(sobreescribe), y en modo append (que añade sin borrar).
+*/
 
+//Crear un archivo y escribir en el mismo(SI modifica y borra lo anterior escrito)
+//usando StreamWriter
+
+try
+{
+    // el segundo argumento del streamreader es un bool para saber si entramos en modo append.
+    StreamWriter sw = new StreamWriter("miArchivo.txt", true);
+    sw.WriteLine("Probando escribir en el archivo...");
+    sw.WriteLine("escribiendo ...");
+    sw.WriteLine("Añadiendo linea... -> append ");
+    sw.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ha ocurrido un error: "+ ex.Message);
+}
+
+//leer un archivo en la carpeta del proyecto
+//usando StreamReader
 try
 {
     StreamReader sr = new StreamReader("miArchivo.txt");
@@ -25,3 +46,4 @@ finally
 {
     Console.WriteLine("Final del try-catch-finally");
 }
+
