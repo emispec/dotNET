00ccd8ed3d39068bcace166f93efda03a5434a77
emispec <emisebastian86@gmail.com>
2023-03-27 11:39:20 -0300

    exceptions

---
+++ P14 Excepciones/Program.cs
@@ -0,0 +1,68 @@
+﻿// Excepciones
+
+try
+{
+    Console.WriteLine($"1 entre 0: {DivisionSegura(1, 0)}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERR:  {ex.Message}");
+}
+//si capturamos la Exception podemos continuar con el programa.
+Console.WriteLine($"1 entre 2: {DivisionSegura(1,2)}");
+
+int[] arr = new int[3];
+arr[0] = 0;
+arr[1] = 1;
+arr[2] = 2;
+//Console.WriteLine(arr[3]); probar error "index outside the bounds of the array"
+
+try
+{
+    Console.WriteLine(arr[3]);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERR: {ex.Message}");
+}
+finally
+{
+    //logica para que el usuario verifique si el valor es correcto.
+    Console.WriteLine("Es correcto el valor que aparece?");
+}
+
+
+
+static double DivisionSegura(double a, double b)
+{
+    if (a == 0 || b == 0)
+    {
+        throw new DivideByZeroException();
+    }
+    return a/b;
+}
+
+
+// NullReferenceException
+try
+{
+    object obj = null;
+    obj.ToString();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERR: {ex.Message}");
+}
+
+
+//ArgumentOutOfRangeException
+try
+{
+    string str = "string";
+    str.Substring(0, str.Length + 1);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERR: {ex.Message}");
+}
+
---
+++ P14 Excepciones/UsuarioNoEncontrado.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P14_Excepciones
+{
+    //creando exception
+    public class UsuarioNoEncontrado : Exception
+    {
+        //constructor
+        public UsuarioNoEncontrado()
+        {
+
+        }
+        //metodos
+        public UsuarioNoEncontrado(string message) : base(message)
+        {
+
+        }
+
+        public UsuarioNoEncontrado(string message, Exception ex) : base(message, ex)
+        {
+            //logica para la exception
+        }
+    }
+}
