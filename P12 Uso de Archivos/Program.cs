4d7b4ae3f09da7578e41993f393ddcc27e2fe8c7
emispec <emisebastian86@gmail.com>
2023-03-26 23:33:00 -0300

    files

---
+++ P12 Uso de Archivos/Program.cs
@@ -0,0 +1,27 @@
+﻿//Crear archivos TXT y realizar operaciones
+
+//importar libreria input/output
+using System.IO;
+
+string texto;
+
+try
+{
+    StreamReader sr = new StreamReader("miArchivo.txt");
+    var linea = sr.ReadLine();
+    while (linea != null)
+    {
+        Console.WriteLine(linea);
+        linea = sr.ReadLine();
+    }
+    sr.Close(); //cerramos el stream asociado al archivo.
+    Console.ReadKey();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ha ocurrido un error: "+ ex.Message);
+}
+finally
+{
+    Console.WriteLine("Final del try-catch-finally");
+}
