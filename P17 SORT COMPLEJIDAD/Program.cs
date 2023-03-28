54587a74d7aecd57cb69bcac3425c3c58a33a0cc
emispec <emisebastian86@gmail.com>
2023-03-28 18:43:59 -0300

    complejidad

---
+++ P17 SORT COMPLEJIDAD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+System.Console.WriteLine("Introduce el numero de elementos: ");
+var N = Convert.ToUInt32(Console.ReadLine());
+
+int[] arr = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+    arr[i] = 1;
+}
+Stopwatch crono = new Stopwatch();
+crono.Start();
+
+for (int j = 0; j < N; j++)
+{
+    for (int k = 0; k < N; k++)
+    {
+        arr[k] = j + k; 
+    }
+}
+crono.Stop();
+System.Console.WriteLine(crono.Elapsed);
